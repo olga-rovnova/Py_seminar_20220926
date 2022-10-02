a6b899dc553c01997a9e0d51fa1ab31acd27dd74
Olga <olga-rovnova@yandex.ru>
2022-10-02 20:06:50 +0300

    main

---
+++ main.py
@@ -0,0 +1,11 @@
+# count = 0
+# read = open('employees_initial.txt', 'r', encoding='utf-8')
+# for line in read:
+#     count += 1
+# read.close()
+# print(count)
+
+
+from ui import choice
+
+choice()
